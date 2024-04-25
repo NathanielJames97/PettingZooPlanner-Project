e407b7612b270557c246fb3ff379f6d6d4516968
Spherox <65167045+NathanielJames97@users.noreply.github.com>
2024-04-25 13:35:34 +0100

    Test method

--- Program.cs
+++ Program.cs
@@ -1,21 +1,4 @@
-﻿/*
-
-- There will be three visiting schools
-    - School A has six visiting groups (the default number)
-    - School B has three visiting groups
-    - School C has two visiting groups
-
-- For each visiting school, perform the following tasks
-    - Randomize the animals
-    - Assign the animals to the correct number of groups
-    - Print the school name
-    - Print the animal groups
-
-
-*/
-
-
-using System;
+﻿using System;
 
 string[] pettingZoo = 
 {
@@ -24,7 +7,26 @@ string[] pettingZoo =
     "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
 };
 
-// RandomizeAnimals();
+RandomizeAnimals();
 // string[,] group = AssignGroup();
 Console.WriteLine("School A");
 // PrintGroup(group);
+
+void RandomizeAnimals() 
+{
+    Random random = new Random();
+
+    for (int i = 0; i < pettingZoo.Length; i++) 
+    {
+        int r = random.Next(i, pettingZoo.Length);
+
+        string temp = pettingZoo[r];
+        pettingZoo[r] = pettingZoo[i];
+        pettingZoo[i] = temp;
+    }
+}
+
+foreach(string animal in pettingZoo) 
+{
+    Console.WriteLine(animal);
+}
