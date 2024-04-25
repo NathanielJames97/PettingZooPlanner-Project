5c21b759ea196e54ceb9564d49963bd3fa86d786
Spherox <65167045+NathanielJames97@users.noreply.github.com>
2024-04-25 13:37:06 +0100

    update

--- Program.cs
+++ Program.cs
@@ -25,3 +25,20 @@ void RandomizeAnimals()
         pettingZoo[i] = temp;
     }
 }
+
+string[,] AssignGroup(int groups = 6) 
+{
+    string[,] result = new string[groups, pettingZoo.Length/groups];
+
+int start = 0;
+
+for (int i = 0; i < groups; i++) 
+{
+    for (int j = 0; j < result.GetLength(1); j++) 
+    {
+        result[i,j] = pettingZoo[start++];
+    }
+}
+
+    return result;
+}
